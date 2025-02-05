c71170dd35d334a229848371f0739f843c81625d
Nidhi Pabbathi <nidhip@cs.utexas.edu>
2025-02-05 02:03:11 -0600

    deploying p1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import MenuItem from './components/MenuItem';
 
-// import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
+import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
 
 // Menu data. An array of objects where each object represents a menu item. Each menu item has an id, title, description, image name, and price.
 // You can use the image name to get the image from the images folder.
@@ -82,10 +82,19 @@ const menuItems = [
 function App() {
   return (
     <div>
-      <h1>Menu</h1>
+      <div className='row'>
+        <div className='col'>
+          <img className='logo' src={`./images/logo.png`} alt={`O's campus cafe`} />
+          <div className='catch_phrase'>
+            <h3>Delicious, From-Scratch Recipes Close at Hand</h3>
+            <h1>The Fresh Choice of UT!</h1>
+          </div>
+        </div>
+      </div>
       <div className="menu">
-        {/* Display menu items dynamicaly here by iterating over the provided menuItems */}
-        <MenuItem title={menuItems[0].title} /> {/* Example for how to use a component */}
+        {menuItems.map((item) => (
+          <MenuItem title={item.title} description={item.description} imageName={item.imageName} price={item.price} />
+        ))}
       </div>
     </div>
   );
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 // This is a functional component that represents a single menu item. It currently takes in the title and displays it in an h2 element.
 // Modify the component to take in all the other properties of a menu item you need and display them in the component.
 // Use bootstrap to style the elements so that it looks like the mockup in the assignment.
 // Hint: You can use the image name to get the image from the images folder.
-const MenuItem = ({ title }) => {
+const MenuItem = ({ title, description, imageName, price }) => {
     return (
         <div>
-            <h2>{title}</h2>
+            <div className="row menu_item">
+                <div className='col'>
+                    <img className='menu_image' src={`./images/${imageName}`} alt={`${title}`} />
+                </div>
+                <div className="col">
+                    <h2 className='title'>{title}</h2>
+                    <p>{description}</p>
+                    <div className="row">
+                        <div className='col'>
+                            <p>{price}</p>
+                        </div>
+                        <div className='col'>
+                            <button className="round"> Add </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
