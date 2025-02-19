b1e63c71d788c550d09203f84963866f02caeb35
Nidhi Pabbathi <nidhip@cs.utexas.edu>
2025-02-19 13:24:36 -0600

    p3 implementation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import MenuItem from './components/MenuItem';
+import React, { useState } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
 
@@ -82,7 +83,7 @@ const MenuHeader = ({ logo, slogan, description }) => {
   return (
     <div className='row'>
       <div className='col'>
-        <img className='logo' src={`./images/${logo}.png`} alt={`O's campus cafe`} />
+        <img className='logo' src={`${process.env.PUBLIC_URL}/images/${logo}.png`} alt={`O's campus cafe`} />
         <div className='catch_phrase'>
           <h3>{slogan}</h3>
           <h1>{description}</h1>
@@ -95,13 +96,67 @@ const MenuHeader = ({ logo, slogan, description }) => {
 
 
 function App() {
+  // sharing state between components: https://react.dev/learn/sharing-state-between-components
+  const [orders, setOrders] = useState(new Array(10).fill(0));
+  const [subtotal, setSubtotal] = useState(0);
+
+  // function passed to menu items in order to update the order amounts
+  // also calculates the subtotal
+  // index: item in the array of menu items to update
+  // isAdding: true if the user is adding the item to cart, false if removing
+  const updateOrder = (index, isAdding) => {
+    // deep copy
+    const newOrders = structuredClone(orders)
+
+    if (isAdding) {
+      newOrders[index]++
+      setSubtotal(subtotal + menuItems[index].price)
+    } else if (newOrders[index] > 0) { // only decreasing if possible (no negatives)
+      newOrders[index]--
+      setSubtotal(subtotal - menuItems[index].price)
+    }
+
+    //update orders
+    setOrders(newOrders)
+  }
+
+  // clear all the items in the cart and the subtotal
+  const clearAll = () => {
+    setOrders(new Array(10).fill(0))
+    setSubtotal(0)
+  }
+
+  // orders the items if there is items in the car
+  const order = () => {
+    if (subtotal === 0) {
+      alert("Error! No Items in Cart.")
+    } else {
+      let orderSummary = ""
+      for (let i = 0; i < menuItems.length; i++) {
+        if (orders[i] > 0) {
+          orderSummary += "- " + menuItems[i].title + " (x" + orders[i] + ")\n"
+        }
+      }
+
+      alert("Order placed!\n\nOrder Summary:\n" + orderSummary + "\nTotal: " + Math.abs(subtotal).toFixed(2))
+      //clear all items from cart once order is placed
+      clearAll()
+    }
+  }
+    
+
   return (
     <div>
       <MenuHeader logo={"logo"} slogan={"Delicious, From-Scratch Recipes Close at Hand"} description={"The Fresh Choice of UT!"}/>
       <div className="menu">
-        {menuItems.map((item) => (
-          <MenuItem title={item.title} description={item.description} imageName={item.imageName} price={item.price} />
+        {menuItems.map((item, index) => (
+          <MenuItem title={item.title} description={item.description} imageName={item.imageName} price={item.price} index={index} orders={orders} updateOrder={updateOrder}/>
         ))}
+      </div>
+      <h1>Subtotal: ${Math.abs(subtotal).toFixed(2)}</h1>
+      <div className="col">
+        <button onClick={clearAll} className="round order"> Clear All </button>
+        <button onClick={order} className="round order"> Order </button>
       </div>
     </div>
   );
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -6,23 +5,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // Modify the component to take in all the other properties of a menu item you need and display them in the component.
 // Use bootstrap to style the elements so that it looks like the mockup in the assignment.
 // Hint: You can use the image name to get the image from the images folder.
-const MenuItem = ({ title, description, imageName, price }) => {
+
+// fixed my bug with onClick with ChatGPT, I was doing updateOrder(index, false) instead of making a function to call () => updateOrder(index, false)
+const MenuItem = ({ title, description, imageName, price, index, orders, updateOrder }) => {
     return (
         <div>
             <div className="row menu_item">
                 <div className='col'>
-                    <img className='menu_image' src={`./images/${imageName}`} alt={`${title}`} />
+                    <img className='menu_image' src={`${process.env.PUBLIC_URL}/images/${imageName}`} alt={`${title}`} />
                 </div>
                 <div className="col">
                     <h2 className='title'>{title}</h2>
-                    <p>{description}</p>
                     <div className="row">
                         <div className='col'>
-                            <p>{price}</p>
+                            <p>{description}</p>
                         </div>
                         <div className='col'>
-                            <button className="round"> Add </button>
+                            <p className='price'>{price}</p>
                         </div>
+                    </div>
+                    <div className="row">
+                        <div className='col'>
+                        </div>
+                        <div className='col'>
+                            <button onClick={() => updateOrder(index, false)} className="round minus"> - </button>
+                            <p className="amount">{orders[index]}</p>
+                            <button onClick={() => updateOrder(index, true)} className="round plus"> + </button>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
